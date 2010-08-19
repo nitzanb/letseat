2e4f765c37a7495b21a70f0b95e5774a97894767
Nitzan Brumer <nitzanb@gmail.com>
2010-08-19 20:28:24 +0300

    all edited files

---
+++ theme/header.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ *      header.php
+ *      
+ *      Copyright 2010 nitzan <nitzan@nitzan-laptop>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title><?php echo nicePageTitle() ?></title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.16" />
+	<link type="text/css" rel="Stylesheet" media="screen" href="/style/style.css"/>
+</head>
+
+<body>
+	
+<div id="wrapper">
+	<div id="header">
+		<h1><?_e('Our happy Site'); ?></h1>
+		<div class ="slogen"><?_e('Our happy slogen');?></div>
+	</div><!-- end header"-->
+
+
+
+
+
---
+++ theme/topnav.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ *      topnav.php
+ *      
+ *      Copyright 2010 nitzan <nitzan@nitzan-laptop>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+?>
+
+<div id="topnav">
+	<ul id="navbar">
+		<li class="active"><a href="<? echo HOME ;?>" title="<?_e('Main Page');?>"><?_e('Main Page');?></a></li>
+		<li class=""><a href="<? echo HOME ;?>account" title="<?_e('My account');?>"><?_e('My account');?></a></li>
+		<li class=""><a href="<? echo HOME ;?>menu" title="<?_e('Menu');?>"><?_e('Menu');?></a></li>
+	
+	</ul>
+	
+	
+
+</div>
